41e8f8cbceac8fd85e1bb4b39c6b7e7d84415b52
Fabrizio Salamanca <gfabrigtrf@gmail.com>
2026-07-14 23:14:50 -0400

    fix: allow dashboard bot controls

--- src/index.js
+++ src/index.js
@@ -89,6 +89,16 @@ await whatsapp.start()
 
 const app = express()
 app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.BOT_CORS_ORIGIN || '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-bot-token')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204)
+    return
+  }
+  next()
+})
 
 app.get('/health', (_req, res) => {
   res.json({
